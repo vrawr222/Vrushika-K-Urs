d858fe77487132272049c9d1db9696b7ddb9becf
vrawr222 <149247235+vrawr222@users.noreply.github.com>
2023-10-28 20:04:55 +0530

    Add files via upload

---
+++ Q1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+
+
+            int[] ar = new int[100];
+
+
+            int lenght, count;
+            count = 0;
+            Console.WriteLine("Enter array length");
+
+            lenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter array elements: ");
+            for (int i = 0; i <= lenght; i++)
+            {
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            int k = ar[0];
+            for (int i = 1; i < lenght; i++)
+            {
+                if (ar[i] == k)
+                    count++;
+
+
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.Write("false");
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+}
+}
---
+++ Q6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Employee
+{
+	public Employee()
+	{
+		string empname, city;
+		double basicsal, DA, HRA;
+
+		void getdata()
+
+        {
+            Console.WriteLine("enter employee name");
+
+            empname = Console.ReadLine();
+            Console.WriteLine("enter city");
+            city = Console.ReadLine();
+            Console.WriteLine("enter basic salary");
+            basicsal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter dearness allowance   ");
+            DA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter house rent ");
+            HRA = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+
+         double calculate()
+        {
+            total = basicsal + basicsal * DA / 100 + basicsal * HRA / 100;
+        } return total();
+;
+        void display()
+        {
+            Console.WriteLine("EMPLOYEE NAME - " + empname "\n" "BASIC SALARY - " + basicsal "\n" "DA - " + DA "\n" "HRA - " + HRA);
+              
+        }
+
+    }
+}
